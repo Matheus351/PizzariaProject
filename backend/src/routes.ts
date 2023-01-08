9161186bb764c76fedfacc7ac29674b1ba32fc9b
Matheus351 <matheusniklaus@gmail.com>
2023-01-08 16:15:00 -0300

    configurando server e prisma :sparkles:

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from "express";
 const router = Router()
 
 router.get('/test', (req:Request, resp:Response)=>{
-  return resp.json({ok:true})
+ throw new Error('Erro ao fazer essa requisiçao')
 })
 
 export {router}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,26 @@
-import express,{ Express } from "express";
+import express,{ NextFunction, Request, Response } from "express";
 import { router } from "./routes";
+import cors from 'cors';
+import 'express-async-errors';
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use(router)//application routes
+
+app.use((err:Error, req:Request, resp:Response, next:NextFunction)=>{
+  
+    if(err instanceof Error){
+        return resp.status(400).json({err:err.message})
+    }
+
+    return resp.status(500).json({
+        status:'error',
+        message:'Internal server error'
+    })
+})
+
 
 app.listen(3333, ()=>console.log('Servidor rodando'))
