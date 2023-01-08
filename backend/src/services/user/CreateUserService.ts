d529c96633e9ccc3f8775aa916a5d7f59e626569
Matheus351 <matheusniklaus@gmail.com>
2023-01-08 17:55:53 -0300

    cadastrando user :sparkles:

--- backend/src/services/user/CreateUserService.ts
+++ backend/src/services/user/CreateUserService.ts
@@ -1,3 +1,4 @@
+import prismaClient from "../../prisma"
 
 interface UserRequest{
   name:string,
@@ -8,7 +9,36 @@ interface UserRequest{
 class CreateUserService{
 
     async execute({name, email, password}:UserRequest){
-      return {ok:true}
+
+        // check if the user sent the email
+        if(!email){
+            throw new Error('Email incorrect')
+        }
+        // checks if a user with that email already exists
+        const userAlreadyExists = await prismaClient.user.findFirst({
+            where:{
+                email:email
+            }
+        })
+
+        if(userAlreadyExists){
+            throw new Error("User already exists")
+        }
+
+        const user = await prismaClient.user.create({
+            data:{
+                name:name,
+                email:email,
+                password:password
+            },
+            select:{
+                id:true,
+                name:true,
+                email:true
+            }
+        })
+
+        return user
     }
 }
 
