82f8c770216e7dfe46a1966c3e320c7dc48204d1
Matheus351 <matheusniklaus@gmail.com>
2023-01-08 18:43:38 -0300

    adicionando login e criptografia de senha :sparkles:

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,8 +1,11 @@
 import { Router} from "express";
 import { UserController } from "./controllers/user/UserController";
+import { AuthUserController } from "./controllers/user/AuthController";
 const router = Router()
 
 //users routes
 router.post('/users', new UserController().handle)
+router.post('/login', new AuthUserController().handle)
+
 
 export {router}
---
+++ backend/src/services/user/AuthUserService.ts
@@ -0,0 +1,33 @@
+import prismaClient from "../../prisma";
+import { compare } from "bcryptjs";
+
+interface AuthRequest{
+  email:string,
+  password:string
+}
+
+class AuthUserService{
+
+    async execute({email, password}:AuthRequest){
+
+      const user = await prismaClient.user.findFirst({
+        where:{
+            email:email
+        }
+      })
+
+      if(!user){
+       throw new Error('User or password incorrect')
+      }
+
+      const passwordMatch = await compare(password, user.password)
+
+      if(!passwordMatch){
+        throw new Error('User or password incorrect')
+      }
+ 
+      return {message:'Logged'}
+    }
+}
+
+export {AuthUserService}
