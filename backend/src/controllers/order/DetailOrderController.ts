e8e90fcc2a1db94657ec8d4d1371ffb8222b1d3a
Matheus351 <matheusniklaus@gmail.com>
2023-01-09 20:33:42 -0300

    buscando detalhes dos orders :sparkles:

---
+++ backend/src/controllers/order/DetailOrderController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { DetailOrderService } from "../../services/order/DetailOrderService";
+
+class DetailOrderController{
+
+    async handle(req:Request, resp:Response){
+
+        const order_id = req.query.order_id as string
+
+        const detailOrderService = new DetailOrderService()
+
+        const orders = await detailOrderService.execute({order_id})
+
+        return resp.json(orders)
+    }
+}
+
+export {DetailOrderController}
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -13,6 +13,7 @@ import { AddItemController } from "./controllers/order/AddItemController";
 import { RemoveItemController } from "./controllers/order/RemoveItemController";
 import { SendOrderController } from "./controllers/order/SendOrderController";
 import { ListOrdersController } from "./controllers/order/ListOrdersController";
+import { DetailOrderController } from "./controllers/order/DetailOrderController";
 
 import uploadConfig from './config/multer'
 import multer from "multer";
@@ -42,5 +43,6 @@ router.post('/order/add', isAuthenticated, new AddItemController().handle)
 router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
 router.put('order/send', isAuthenticated, new SendOrderController().handle)
 router.get('/allorders', isAuthenticated, new ListOrdersController().handle)
+router.get('/order/detail', isAuthenticated, new DetailOrderController().handle)
 
 export {router}
