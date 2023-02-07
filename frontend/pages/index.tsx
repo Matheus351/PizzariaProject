3f0a973c43425e728f9cc62ba2371ca543ad62e2
Matheus351 <matheusniklaus@gmail.com>
2023-02-07 20:57:36 -0300

    criando tela de login

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,4 +1,10 @@
 import Head from "next/head"
+import Image from "next/image"
+import logo from '../public/logo.svg'
+import styles from '../styles/home.module.scss'
+
+import {Input, TextArea} from '../components/ui/Input'
+import Button from '../components/ui/Button'
 
 export default function Home() {
   return (
@@ -6,6 +12,23 @@ export default function Home() {
     <Head>
       <title>Login Page</title>
     </Head>
+    <div className={styles.containerCenter}>
+        <Image alt="logo" src={logo} />
+
+        <div className={styles.login}>
+          <form>
+            <Input type="text" placeholder="Digite seu nome"/>
+            <Input type="password" placeholder="Digite sua senha"/>
+            <Button 
+            type='submit'
+            loading={false}>
+              Acessar 
+            </Button>
+          </form>
+
+          <a className={styles.text} href="">Não possui uma conta? Cadastre-se</a>
+        </div>
+    </div>
     </>
 
   )
