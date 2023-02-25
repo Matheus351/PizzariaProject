b754248eba96b51eb19c96e902bf664ae7772422
Matheus351 <matheusniklaus@gmail.com>
2023-02-25 00:24:06 -0300

    configurando cadastro

--- frontend/pages/signup/index.tsx
+++ frontend/pages/signup/index.tsx
@@ -6,8 +6,27 @@ import styles from '../../styles/home.module.scss'
 import { Input, TextArea } from "@/components/ui/Input"
 import Button from "@/components/ui/Button"
 import Link from "next/link"
+import { FormEvent, useState } from "react"
 
 export default function Signup() {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  async function handleSignUp(event:FormEvent) {
+
+     event.preventDefault();
+
+     if(name===''||email===''||password===''){
+       alert('Preencha todos os campos');
+       return
+     }
+     setLoading(true);
+
+  }
+
   return (
     <>
       <Head>
@@ -18,20 +37,19 @@ export default function Signup() {
        
         <div className={styles.login}>
         <h1 className={styles.h1}>Criando sua conta</h1>
-          <form>
-            <Input type="text" placeholder="Digite seu nome" />
-            <Input type="email" placeholder="Digite seu email" />
-            <Input type="password" placeholder="Digite sua senha" />
+          <form onSubmit={handleSignUp}>
+            <Input onChange={(e)=>setName(e.target.value)} type="text" placeholder="Digite seu nome" value={name}/>
+            <Input onChange={(e)=>setEmail(e.target.value)} type="email" placeholder="Digite seu email" value={email}/>
+            <Input onChange={(e)=>setPassword(e.target.value)} type="password" placeholder="Digite sua senha" value={password}/>
             <Button
               type='submit'
-              loading={false}>
+              loading={loading}>
               Cadastrar
             </Button>
           </form>
 
           <Link className={styles.text} href={'/'}>
             Já possui uma conta? Faça Login
-            {/* <a className={styles.text}>Não possui uma conta? Cadastre-se</a> */}
           </Link>
 
 
